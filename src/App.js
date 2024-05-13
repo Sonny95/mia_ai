29068020c5bc411f449a00684a67f67fc96bb2d9
Sonny95 <thswlgml9598@gmail.com>
2024-05-13 20:33:36 +1000

    layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
@@ -6,8 +6,14 @@ function App() {
     <div className="bg-black h-screen flex flex-col items-center justify-center font-mono text-white">
       <p className="mb-10 text-3xl">Any question about Mia?</p>
       <div className="w-1/2 h-4/5 bg-gray-800 rounded-lg flex flex-col justify-between">
-        <div>Hi</div>
-        <input placeholder="Message Mia..." className="bg-gray-600 rounded-lg h-14 mt-auto"></input>
+        <p>Mia's Bot:</p>
+        <div className="bg-gray-600 rounded-lg h-14 mt-auto justify-between flex items-center">
+          <input
+            placeholder="Message Mia..."
+            className="bg-gray-600 rounded-lg h-14 mt-auto ml-4"
+          ></input>
+          <button className="bg-gray-800 h-10 rounded-lg w-16 mr-4">send</button>{" "}
+        </div>
       </div>
     </div>
   );
