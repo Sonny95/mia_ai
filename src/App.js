86edddc0a1d10a08551c13ebb96b48a21a0621ba
Sonny95 <thswlgml9598@gmail.com>
2024-05-14 17:23:47 +1000

    connect to server

--- src/App.js
+++ src/App.js
@@ -1,18 +1,63 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  //user
+  const [inputText, setInputText] = useState("");
+  //ai
+  const [response, setResponse] = useState("");
+  const [assistantId, setAssistantId] = useState("");
+  const [threadId, setThreadId] = useState("");
+
+  //assisstant id and thread id
+  useEffect(() => {
+    fetchAssistantAndThreadId();
+  }, []);
+
+  // get assisant id and thread id
+  const fetchAssistantAndThreadId = async () => {
+    try {
+      const res = await axios.get("http://localhost:8000/init");
+      const { assistantId, threadId } = res.data;
+      setAssistantId(assistantId);
+      setThreadId(threadId);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const sendMessage = async () => {
+    try {
+      const res = await axios.post("http://localhost:8000/ai", {
+        message: inputText,
+        assistantId: assistantId,
+        threadId: threadId,
+      });
+      setResponse(res.data);
+      console.log(res, "setResponse");
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <div className="bg-black h-screen flex flex-col items-center justify-center font-mono text-white">
       <p className="mb-10 text-3xl">Any question about Mia?</p>
       <div className="w-1/2 h-4/5 bg-gray-800 rounded-lg flex flex-col justify-between">
-        <p>Mia's Bot:</p>
+        <p className="float-right">You:{inputText}</p>
+        <p>Mia's Bot:{response}</p>
+
         <div className="bg-gray-600 rounded-lg h-14 mt-auto justify-between flex items-center">
           <input
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
             placeholder="Message Mia..."
-            className="bg-gray-600 rounded-lg h-14 mt-auto ml-4"
+            className="bg-gray-600 rounded-lg h-14 mt-auto ml-4 w-full"
           ></input>
-          <button className="bg-gray-800 h-10 rounded-lg w-16 mr-4">send</button>{" "}
+          <button onClick={sendMessage} className="bg-gray-800 h-10 rounded-lg w-16 mr-4">
+            send
+          </button>
         </div>
       </div>
     </div>
